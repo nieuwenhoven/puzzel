7401c0b41847099498166f9b7bdd87987a19e33d
Frans <frans.van.nieuwenhoven@gmail.com>
2024-07-20 15:52:41 +0200

    wat aanpassingen aan het uiterlijk en poging om er een pwa van te maken

---
+++ serviceWorker.js
@@ -0,0 +1,31 @@
+const staticPyPWA = "dev-pypwa-v4"
+const assets = [
+    // "",
+    // "/",
+    "/puzzel.css",
+    "/puzzel.js",
+    "/marloes_en_frans.jpg",
+    "/pwa-scaffold.js",
+    "/serviceWorker.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticPyPWA).then(cache => {
+            cache.addAll(assets).then(r => {
+                console.log("Cache assets downloaded");
+            }).catch(err => console.log("Error caching item", err))
+            console.log(`Cache ${staticPyPWA} opened.`);
+        }).catch(err => console.log("Error opening cache", err))
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        }).catch(err => console.log("Cache fetch error: ", err))
+    )
+})
